7205fcc70c7229a5748caf0b7d526439f702158b
Magnus Bäck <magnus@noun.se>
2015-11-27 21:14:36 +0100

    Remove mentions of the non-existent server mode.
    The plugin currently has no server mode (although it should
    have -- see issue #2) and the "mode" option shouldn't indicate
    that anything but client mode works. Also make the "url" option
    required and remove the 0.0.0.0 default value which never made
    any sense.
    
    Because we no longer allow server mode to be configured, there
    is a chance that people have set "mode => server" and got away
    with it since the plugin always functioned in client mode
    regardless of the value of the "mode" option. Such users will
    have a bad time if they upgrade, so to be safe we bump the major
    version to 3.0.0 and make a note in the changelog.
    
    Fixes #8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+## 3.0.0
+ - The "mode" option's "server" value has been removed since server mode
+   isn't supported (and never has been). This could potentially affect
+   existing users who have had "mode => server" set but in reality have
+   been using client mode all along.
+ - The "url" option is now mandatory.
+
 ## 2.0.0
  - Plugins were updated to follow the new shutdown semantic, this mainly allows Logstash to instruct input plugins to terminate gracefully, 
    instead of using Thread.raise on the plugins' threads. Ref: https://github.com/elastic/logstash/pull/3895
--- spec/inputs/websocket_spec.rb
+++ spec/inputs/websocket_spec.rb
@@ -1,1 +1,25 @@
 require "logstash/devutils/rspec/spec_helper"
+require "logstash/inputs/websocket"
+
+describe LogStash::Inputs::Websocket do
+
+  context "when url option isn't set" do
+    subject { LogStash::Inputs::Websocket.new() }
+
+    it "should raise exception" do
+      expect { subject.register }.to raise_error(LogStash::ConfigurationError)
+    end
+  end
+
+  context "when attempting to enable server mode" do
+    subject {
+      LogStash::Inputs::Websocket.new("url" => "http://example.com",
+                                      "mode" => "server")
+    }
+
+    it "should raise exception" do
+      expect { subject.register }.to raise_error(LogStash::ConfigurationError)
+    end
+  end
+
+end
